567b652e3c02b9b2c4c390bc5152529f39118068
Casey Ng <55763133+cng27@users.noreply.github.com>
2025-04-12 00:07:02 -0700

    add boxplot function

---
+++ dashboard/boxPlot.py
@@ -0,0 +1,12 @@
+import plotly.graph_objects as go
+import plotly.express as px
+import pandas as pd
+import numpy as np
+
+
+def box_plot(df, x, y):
+    fig = px.box(df, x=x, y=y, color=x,
+                 title=f'{y} Scores vs {x.title().replace("_", " ")}',
+                 labels={x: x.title(), y: y})
+    fig.update_layout(showlegend=False, title_x=0.5)
+    return fig
