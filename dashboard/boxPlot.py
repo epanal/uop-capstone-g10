14692abfe1f3da533fd81b3e10b1aef53eeca5c8
Casey Ng <55763133+cng27@users.noreply.github.com>
2025-04-19 01:02:15 -0700

    change to dark theme

--- dashboard/boxPlot.py
+++ dashboard/boxPlot.py
@@ -8,5 +8,5 @@ def box_plot(df, x, y):
     fig = px.box(df, x=x, y=y, color=x,
                  title=f'{y} Scores vs {x.title().replace("_", " ")}',
                  labels={x: x.title(), y: y})
-    fig.update_layout(showlegend=False, title_x=0.5)
+    fig.update_layout(showlegend=False, title_x=0.5, template="plotly_dark",)
     return fig
